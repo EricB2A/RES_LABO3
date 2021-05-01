934df8a0fc94c1c459516bef8aad97fc7d9cf0be
Ilias <igoujgali@gmail.com>
2021-05-01 23:26:39 +0200

    Commentaire & finalisation de email, close #5

--- src/main/java/heig/model/prank/Email.java
+++ src/main/java/heig/model/prank/Email.java
@@ -4,9 +4,34 @@ import heig.model.email.Group;
 import heig.model.email.Message;
 import heig.model.email.Person;
 
+/**
+ * Interface qu'un e-mail doit posséder
+ *
+ * @author Eric Bousbaa & Ilias Goujgali
+ * @version 1.0
+ */
 public interface Email {
-    public Person getSender();
-    public Group getReceivers();
-    public Group getCCReceivers();
-    public Message getMessage();
+    /**
+     * Retourne l'expéditeur de l'email
+     * @return Person destinataire
+     */
+    Person getSender();
+
+    /**
+     * Retourne le groupe de  destinataires du message
+     * @return Group de destinataires
+     */
+    Group getReceivers();
+
+    /**
+     * Retourne le groupe de destinataires CC du message
+     * @return Group des destinataires CC
+     */
+    Group getCCReceivers();
+
+    /**
+     * Retourne le Message ( titre & contenu ) de l'email
+     * @return Message de l'email
+     */
+    Message getMessage();
 }
