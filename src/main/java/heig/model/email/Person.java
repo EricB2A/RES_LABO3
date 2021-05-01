28820f76bfc7afd0d9689367e7c6a7cfbd07f51d
Ilias <igoujgali@gmail.com>
2021-05-01 12:51:03 +0200

    Création de la classe person #1

--- src/main/java/heig/model/email/Person.java
+++ src/main/java/heig/model/email/Person.java
@@ -1,4 +1,12 @@
 package heig.model.email;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@AllArgsConstructor
 public class Person {
+    @Getter
+    @Setter
+    private String email;
 }
