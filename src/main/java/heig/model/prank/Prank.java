418d35ab9a76fd69a491f17091047820140ccbf7
Ilias <igoujgali@gmail.com>
2021-05-01 23:26:11 +0200

    Commentaire & finalisation de Prank, close #5

--- src/main/java/heig/model/prank/Prank.java
+++ src/main/java/heig/model/prank/Prank.java
@@ -6,20 +6,28 @@ import heig.model.email.Person;
 import lombok.Getter;
 import lombok.Setter;
 
+/**
+ * Classe repésentant un prank par email. Ce dernier est composé d'un expéditeur, d'un groupe de destinataire, d'un groupe CC
+ * et d'un message.
+ *
+ *
+ * @author Eric Bousbaa & Ilias Goujgali
+ * @version 1.0
+ */
 public class Prank implements Email {
     @Getter
     @Setter
-    private Person sender;
+    private Person sender; // Expéditeur
 
     @Getter
     @Setter
-    private Group receivers;
+    private Group receivers; // Destinataires
 
     @Getter
     @Setter
-    private Group CCReceivers;
+    private Group CCReceivers; // Destinataires CC
 
     @Getter
     @Setter
-    private Message message;
+    private Message message; // Message de l'e-mail (titre & message)
 }
