6e5af2a9adb6b9bd81568bcd380d0a04f82f729f
Ilias <igoujgali@gmail.com>
2021-05-01 13:21:41 +0200

    Ajout d'un main

--- src/main/java/heig/Main.java
+++ src/main/java/heig/Main.java
@@ -1,9 +1,36 @@
 package heig;
 
+import heig.exception.InvalidPrankConfiguration;
+import heig.config.ConfigManager;
+import heig.model.prank.Email;
+import heig.model.prank.PrankGenerator;
+import heig.smtp.SmtpClient;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.logging.Logger;
+
 public class Main {
 
+    private static final Logger LOG = Logger.getLogger(Main.class.getName());
+
     public static void main(String[] args) {
-	// write your code here
-        System.out.println("Hello puto");
+
+
+        ConfigManager config = null;
+        try {
+            config = ConfigManager.getInstance();
+
+            PrankGenerator prankGenerator = new PrankGenerator();
+            List<Email> pranks = prankGenerator.generatePranks(config.getNbGroups(), config.getNbMembers(), config.getVictims(), config.getCcWitnesses(),config.getMessages());
+
+            SmtpClient smtpClient = new SmtpClient(config.getSmtpServerIp(), config.getSmtpServerPort());
+            smtpClient.sendEmails(pranks);
+
+        } catch (InvalidPrankConfiguration | IOException e) {
+            LOG.severe("AN ERROR OCCURRED");
+            LOG.severe(e.getMessage());
+            System.exit(1);
+        }
     }
 }
