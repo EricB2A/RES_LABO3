b85f874a4378e868e6bbd62ed1c12c9ba937c123
Ilias <igoujgali@gmail.com>
2021-05-01 23:23:32 +0200

    Commentaire & finalisation de Person. Close #1

--- src/main/java/heig/model/email/Person.java
+++ src/main/java/heig/model/email/Person.java
@@ -4,9 +4,18 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 
+/**
+ * Classe représantant une personne. Note: pour le moment seule l'adresse e-mail nous intéresse, c'est pour cette raison
+ * qu'aucun autre attirbut n'est présent. Il aurait pu être envisable d'ajouter le nom et prénom des personnes afin d'avoir
+ * des e-mails un peu plus personnels.
+ *
+ * @author Eric Bousbaa & Ilias Goujgali
+ * @version 1.0
+ */
 @AllArgsConstructor
 public class Person {
+
     @Getter
     @Setter
-    private String email;
+    private String email; // Email de la personne
 }
