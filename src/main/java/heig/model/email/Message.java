9177f943e68ac4e692c03a3196e6069d1fbe31a2
Ilias <igoujgali@gmail.com>
2021-05-01 23:24:05 +0200

    Commentaire & finalisation de Message ,closes #3

--- src/main/java/heig/model/email/Message.java
+++ src/main/java/heig/model/email/Message.java
@@ -5,14 +5,21 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+/**
+ * Classe représentant un message composé d'un titre et d'un contenu du message
+ *
+ *  @author Eric Bousbaa & Ilias Goujgali
+ *  @version 1.0
+ */
 @NoArgsConstructor
 @AllArgsConstructor
 public class Message {
-    @Getter
-    @Setter
-    private String subject;
 
     @Getter
     @Setter
-    private String content;
+    private String subject; // Titre
+
+    @Getter
+    @Setter
+    private String content; // Contenu du message
 }
